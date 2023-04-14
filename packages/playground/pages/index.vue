d0cfb68cd799b7320fab7e4de9c25e7b79e75be9
Noah <noah4882@163.com>
2023-04-14 15:21:50 +0800

    chore: add demo

--- packages/playground/pages/index.vue
+++ packages/playground/pages/index.vue
@@ -13,17 +13,45 @@ interface Res {
   success: boolean
 }
 
-interface UseFetchOptions<Res> {}
+interface Code {
+  code: number
+  data: {
+    code: number
+  }
+  error: []
+  message: string
+  success: boolean
+}
 
 const bannerData = ref<Res | null>(null)
 const getBannerData = async () => {
-  const { data, pending, error, refresh } = await useFetch('/api/auth/send-code', {
-    method: 'post',
-    body: {
-      account: 15622274882,
-      code_type: 2,
-    },
-  })
+  // 注入响应数据的类型：Res
+  const { data, pending, error, refresh } = await useFetch<Res>('/api/setting/banner')
+
+  // 可以获得类型提示
+  console.log(data.value?.data[0].id)
+}
+const getCode = async () => {
+  /**
+   * https://github.com/nuxt/nuxt/issues/19077
+   * 根据 issue 描述，这是 Nuxt v3.2.0 的 bug，即：
+   * 在非 get 请求的情况下，注入类型后 TS 报错，无法获得类型提示
+   * 临时方案，post 请求为例：useFetch<Code> 修改为 useFetch<Code, Error, string, 'post' | 'get'> 可重新获得类型提示
+   * -------------------------- */
+
+  // 注入响应数据的类型：Code
+  const { data, pending, error, refresh } = await useFetch<Code, Error, string, 'post' | 'get'>(
+    '/api/auth/send-code',
+    {
+      method: 'post',
+      body: {
+        account: 13813801380,
+        code_type: 2,
+      },
+    }
+  )
+
+  // 可以获得类型提示
   console.log(data.value?.data.code)
 }
 const out = () => {
@@ -36,6 +64,7 @@ const out = () => {
     <div class="ys-playground__banner">
       <div class="ys-playground__banner-button">
         <div class="ys-playground__banner-button-get" @click="getBannerData()">测试反向代理</div>
+        <div class="ys-playground__banner-button-get" @click="getCode()">测试 POST 请求</div>
       </div>
       <img v-if="bannerData" :src="bannerData?.data[0].file_url" alt="banner" />
     </div>
