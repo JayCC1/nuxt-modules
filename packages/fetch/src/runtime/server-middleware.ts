eedb83a12fd06aa2a59fe0b04ea159a81da9d594
Noah <noah4882@163.com>
2023-04-24 13:01:35 +0800

    fix(fetch): use `proxyRequest` instead of `$fetch` to resolve Internal Server Error (#3)

--- packages/fetch/src/runtime/server-middleware.ts
+++ packages/fetch/src/runtime/server-middleware.ts
@@ -1,11 +1,4 @@
-import {
-  defineEventHandler,
-  getRequestHeaders,
-  readBody,
-  parseCookies,
-  getMethod,
-  getQuery,
-} from 'h3'
+import { defineEventHandler, getProxyRequestHeaders, parseCookies, proxyRequest } from 'h3'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: resolved with Nuxt
 import { useRuntimeConfig } from '#imports'
@@ -17,23 +10,22 @@ const { nuxtFetch } = useRuntimeConfig()
 export default defineEventHandler(async (event: H3Event) => {
   if (event.node.req.url?.startsWith(nuxtFetch.baseApi)) {
     const url = `${nuxtFetch.serviceUrl}${event.node.req.url}`
-    const method = getMethod(event)
-    const query = getQuery(event)
-    const headers = getRequestHeaders(event)
+    const headers = getProxyRequestHeaders(event)
     const cookies = parseCookies(event)
-    let body = null
     headers['Authorization'] = cookies[nuxtFetch.authorization || 'access_token']
-    delete headers['host']
-    if ('GET' !== method.toUpperCase()) {
-      body = await readBody(event)
-    }
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore: resolved with Nuxt
-    return $fetch(url, {
-      method,
-      params: query,
+
+    /**
+     * fix: Use proxyRequest instead of $fetch to resolve Internal Server Error (#3)
+     *
+     * note:
+     * 当删除 headers 中的 connection 字段之后错误不再出现
+     * 也许是 h3 的设定或 bug
+     * 在 https://github.com/unjs/h3/issues/375 中有提到相关的错误
+     *
+     * TODO: 目前问题如何出现的尚未明确，仍需注意此处会发生未知的错误。
+     * -------------------------- */
+    return proxyRequest(event, url, {
       headers,
-      body,
     })
   }
 })
