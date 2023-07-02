92ab11717689216b7f4ad5fdca18d1c0ca54cffd
Noah <noah4882@163.com>
2023-07-03 00:49:14 +0800

    chore: update playground config

--- packages/playground/nuxt.config.ts
+++ packages/playground/nuxt.config.ts
@@ -1,16 +1,22 @@
 export default defineNuxtConfig({
-  modules: ['@spruce-hub/nuxt-fetch', '@spruce-hub/nuxt-route'],
-  nuxtRoute: {
-    authPath: ['/order'],
-    loginPath: '/login',
-    tokenName: 'access_token',
-    excludePath: ['/login', '/register'],
-  },
-  runtimeConfig: {
-    nuxtFetch: {
-      baseApi: '/api',
-      serviceUrl: 'http://zfl.online.dev.fyunshan.com',
-      authorization: 'access_token',
-    },
-  },
+  modules: [
+    [
+      '@spruce-hub/nuxt-fetch',
+      {
+        apiBase: '/api',
+        apiHostEnv: 'API_HOST',
+        apiHostUrl: 'http://zflcnapi.online.dev.fyunshan.com/',
+        cookieName: 'access_token',
+      },
+    ],
+    [
+      '@spruce-hub/nuxt-route',
+      {
+        authPath: ['/order'],
+        loginPath: '/login',
+        cookieName: 'access_token',
+        excludePath: ['/login', '/register'],
+      },
+    ],
+  ],
 })
