1895873802dd19a4ce92da1b6c357bd114299812
Noah <noah4882@163.com>
2023-08-12 11:09:29 +0800

    fix(fetch): 删除默认配置

--- packages/fetch/src/config.ts
+++ packages/fetch/src/config.ts
@@ -1,9 +1,3 @@
 import type { Options } from './types'
 
-export const defaults: Record<string, Partial<Options>> = {
-  '/api': {
-    apiHostEnv: '',
-    apiHostUrl: '',
-    cookieName: '',
-  },
-}
+export const defaults: Record<string, Partial<Options>> = {}
